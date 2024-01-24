9cc91b6475c1b6ea2c41546a085cc4ac7edcba5d
Byron Kimani <byronkimani@gmail.com>
2024-01-24 05:07:17 +0300

    chore: add github workflows
    - add github workflows

--- src/main/java/com/byronkimani/CreditCardManger/model/ErrorDetails.java
+++ src/main/java/com/byronkimani/CreditCardManger/model/ErrorDetails.java
@@ -2,9 +2,6 @@ package com.byronkimani.CreditCardManger.model;
 
 import java.time.LocalDateTime;
 
-/**
- * Created by Eugene Devv on 23 Jan, 2024
- */
 public class ErrorDetails {
     private LocalDateTime timestamp;
     private String message;
